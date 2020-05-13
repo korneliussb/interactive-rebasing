0c4831e8f9d74de73aabcbc629b3ae9212b01088
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-05-13 23:43:05 +0700

    Work In Progress
    finish up person and job classes

--- Job.php
+++ Job.php
@@ -1,4 +1,14 @@
 <?php
 class Job
 {
+    protected $title;
+    public function worksAt($title)
+    {
+        $this->title =  new Job($title);
+    }
+
+    public function title()
+    {
+        return $this->title;
+    }
 }
